b9d74f6a9f7397f44e74b965aacc22f2a0fbd1d5
Dman2740 <darioayala98@gmail.com>
2019-05-21 15:05:36 -0400

    Update JsonReader.py

--- JsonReader.py
+++ JsonReader.py
@@ -1,6 +1,14 @@
 #import packages
 import json
 import os
+import webbrowser
+
+new=2;
+
+#This is in order to get the user profile from the shortcode in the json
+url="http://www.instagram.com/p/"
+
+
 
 #See where the json file is and we use the system call because it is more efficient
 CWD=os.getcwd()
@@ -11,23 +19,17 @@ CONFIG_PROPERTIES={}
 
 
 #Open the json file, parse file, and store them in dictionary
-#With is a good method because it will always close 
+#With is a good method because it will always close
+#We use load because we are reading from a json file
 try:
-    with open(JSON_CONFIG_FILE_PATH) as data_file:
+    with open(JSON_CONFIG_FILE_PATH,"r") as data_file:
         CONFIG_PROPERTIES=json.load(data_file)
-        data_serialized=json.dumps(CONFIG_PROPERTIES)
-        #Use dumps in order to convert the dictionary to a string
-        #for CONFIG_PROPERTIES in CONFIG_PROPERTIES.keys():
-         #   print(CONFIG_PROPERTIES)
-        shortcode="shortcode"
-        for shortcode in CONFIG_PROPERTIES:
-            print(shortcode)
+        shortcode=(CONFIG_PROPERTIES['node']['shortcode'])
+        newUrl=url+shortcode
+        webbrowser.open(newUrl,new=new)
+        
         
 except IOError as e:
     print (e)
-    print ('IOError:Unable to open JSON file')
+    print ("IOError:Unable to open JSON file")
     exit(1)
-print (CONFIG_PROPERTIES)
-print (data_serialized)
-print (type(CONFIG_PROPERTIES))
-print (type(data_serialized))
